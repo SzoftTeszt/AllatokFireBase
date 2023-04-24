79130b76528a485be057189961bf93277100ae5c
JagerAttila <kitalalt@gmail.com>
2023-04-24 19:50:31 +0200

    0424end

--- src/app/part/list-animals/list-animals.component.ts
+++ src/app/part/list-animals/list-animals.component.ts
@@ -11,17 +11,30 @@ import { map } from 'rxjs';
 export class ListAnimalsComponent {
  ujAllat: any={};
  oszlopok=["key", "nev", "fajta", "kor"]
- 
+ allatok:any;
  constructor(private base:BaseService){
   this.base.getAll().snapshotChanges()
   .pipe(map( changes=> changes.map(
       c=>({key: c.payload.key, ...c.payload.val()})
     ))
-  ).subscribe(data=> console.log(data))
+  ).subscribe(data=> {this.allatok=data; console.log("Az adatok frissültek")})
  }
 
  newAnimal(body:Animal){
   this.base.create(body).then(()=>console.log("Új állat rögzítve!"))
   .catch(()=>console.log("Hiba az új állatka rögzítésénél!"))
  }
+
+ delete(body:any){
+    this.base.delete(body.key).then().catch(err=>console.log(err));
+ }
+
+ deleteAll(){
+    this.base.deleteAll().then().catch(err=>console.log(err));
+ }
+
+ update(body:any){
+    this.base.update(body.key, body).then().catch(err=>console.log(err));
+ }
+
 }
